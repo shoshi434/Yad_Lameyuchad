38b24c8d233d3aeaf409910f85216f7000387192
shoshi434 <shoshi.work.34@gmail.com>
2026-03-08 17:53:58 +0200

    Update production address

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -2,7 +2,8 @@ const allowedOrigins = [
     'http://localhost:3000',
     'http://localhost:5500',
     'http://localhost:3001',
-    'http://localhost:3002'
+    'http://localhost:3002',
+    'https://yad-lameyuchad.onrender.com'
 ]
 
 // Add production URL if exists
